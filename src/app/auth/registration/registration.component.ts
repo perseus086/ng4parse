38a5d5908f51d0f793a485e4574cf8285956ba5c
perseus086 <perseus086@gmail.com>
2017-09-06 20:55:11 -0500

    refactor(registration): change from template to reactive forms

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ParseService } from '../../core/parse/parse.service';
 import { Router } from '@angular/router';
 import { MdSnackBar } from '@angular/material';
+import {
+  FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl,
+  FormGroupDirective, NgForm
+} from '@angular/forms';
+
+function passwordValidator(pass1: string, pass2: string): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} => {
+    const passNotMatch = control.get(pass1).value !== control.get(pass2).value;
+    return passNotMatch ? {'passwordNotMatch': {'value': true} }: null;
+  };
+}
 
 @Component({
   selector: 'cb-registration',
@@ -10,19 +21,24 @@ import { MdSnackBar } from '@angular/material';
 })
 
 export class RegistrationComponent implements OnInit{
-  private model: RegistrationModel;
 
-  constructor(private parseService: ParseService, private router: Router, public snackBar: MdSnackBar) {}
+  registrationForm: FormGroup;
+
+  constructor(private parseService: ParseService, private router: Router, public snackBar: MdSnackBar,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.model = new RegistrationModel('', '', '', '');
-    console.log('on Registration');
+    this.registrationForm = this.formBuilder.group({
+      //We are using userName the same as email, if diffetent change this validator
+      userName: [null, Validators.compose([Validators.required, Validators.email])],
+      password: [null, Validators.compose([Validators.required, Validators.minLength(6)])],
+      passwordRepeat: [null, Validators.compose([Validators.required, Validators.minLength(6)])]
+    }, { validator: passwordValidator('password', 'passwordRepeat') })
   }
 
-  onSubmit() {
+  onSubmit(formRef) {
     //Because we will use the email as username in this case
-    console.log(this.model);
-    this.parseService.register(this.model.userName, this.model.password, this.model.userName)
+    this.parseService.register(formRef.userName, formRef.password, formRef.userName)
       .then( () => {
         this.router.navigate(['']);
       })
@@ -40,13 +56,9 @@ export class RegistrationComponent implements OnInit{
   login() {
     this.router.navigate(['auth', 'login']);
   }
-}
 
-class RegistrationModel {
-  constructor(
-    public userName: string,
-    public email: string,
-    public password: string,
-    public passwordRepeat: string,
-  ){};
+  passwordMatcher(control: FormControl, form: FormGroupDirective | NgForm): boolean {
+    return control.touched && form.value.password !== control.value;
+  }
+
 }
